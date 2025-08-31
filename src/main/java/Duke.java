210da094c73b00699d61493ca23e04170aa95b81
Leyli Jalilova <l.jalilova235@gmail.com>
2025-09-01 00:02:25 +0800

    Adjust default message.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = """
+                ____________________________________________________________
+                Hello! I'm Joel.
+                What can I do for you?
+                ____________________________________________________________
+                Bye. Hope to see you again soon!
+                ____________________________________________________________
+                """;
+        System.out.println(logo);
     }
 }
