4e0f4eb830cb182ff3168d037c08f2f3c6413d03
Leyli Jalilova <l.jalilova235@gmail.com>
2025-09-01 03:56:11 +0800

    Add list functionality to chatbot.

--- src/main/java/Joel.java
+++ src/main/java/Joel.java
@@ -8,12 +8,26 @@ public class Joel {
                 """;
         String greeting = divider + "Hello! I'm Joel.\nWhat can I do for you?\n" + divider;
         String exitMessage = divider + "Bye. Hope to see you again soon!\n" + divider;
+        String[] tasks = new String[100];
+        int currentListIndex = 0;
 
         System.out.println(greeting);
         String userInput = scanner.nextLine();
 
         while (!userInput.equals("bye")) {
-            System.out.println(divider + userInput + "\n" + divider);
+            if (userInput.equals("list")) {
+                if (tasks[0] == null) {
+                    System.out.println("Whoops! Your list is empty.");
+                } else {
+                    for (int i = 0; i < currentListIndex; i++) {
+                        System.out.println((i + 1) + ". " + tasks[i]);
+                    }
+                }
+            } else {
+                tasks[currentListIndex] = userInput;
+                currentListIndex++;
+                System.out.println(divider + "added: " + userInput + "\n" + divider);
+            }
             userInput = scanner.nextLine();
         }
 
